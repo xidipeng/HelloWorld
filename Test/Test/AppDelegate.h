761e929d4a251372d430cc454b85f9361defc09d
CD <CD@mini.local>
2018-12-10 15:37:07 +0800

    test

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by CD on 2018/12/10.
+//  Copyright © 2018 CD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
